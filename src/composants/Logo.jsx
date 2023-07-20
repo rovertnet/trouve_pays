224bab41b5b9e9a231efdcbf2ac12fec02d8a130
Robert Matundu <matundukabamba@gmail.com>
2023-07-20 18:48:12 +0200

    un autre commit

--- src/composants/Logo.jsx
+++ src/composants/Logo.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Logo = () => {
   return (
     <div className="logo">
-        <img src="" alt="logo" />
+        <img src="./logo192.png" alt="logo" />
         <h3>Trouve Pays</h3>
     </div>
   )
