ec6e851f4656def7d7f58a6f7794d0afec9dc7ed
Robert Matundu <matundukabamba@gmail.com>
2023-07-20 18:17:59 +0200

    un autre commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
+import "./styles/index.scss";
 
 
 const App = () => {
