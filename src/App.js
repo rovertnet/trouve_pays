58c54f4fc927ad48902a658b86dfe7643b2201a2
Robert Matundu <matundukabamba@gmail.com>
2023-07-20 15:50:18 +0200

    un autre commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
 
 
 const App = () => {
     return (
-        <div>App</div>
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/about' element={<About />} />
+            </Routes>
+        </BrowserRouter>
     )
 }
 
