0422490232697bb77c32b5b02c14aee7ed9c3d98
Robert Matundu <matundukabamba@gmail.com>
2023-07-20 16:12:49 +0200

    un autre commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import Navigation from '../composants/Navigation'
 
 const Home = () => {
   return (
     <div>
+        <Navigation />
         <h1>Accueil</h1>
     </div>
   )
