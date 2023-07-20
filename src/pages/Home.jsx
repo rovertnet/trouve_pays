8eb37cdc9125f1bed8db5fd45a2117651ccde003
Robert Matundu <matundukabamba@gmail.com>
2023-07-20 18:44:47 +0200

    un autre commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Navigation from '../composants/Navigation'
+import Logo from '../composants/Logo'
 
 const Home = () => {
   return (
     <div>
+        <Logo />
         <Navigation />
         <h1>Accueil</h1>
     </div>
