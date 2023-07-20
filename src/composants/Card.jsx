3178359caf0ee9e8a89e9b1869c30debe0fa1a75
Robert Matundu <matundukabamba@gmail.com>
2023-07-21 00:21:20 +0200

    un autre commit

--- src/composants/Card.jsx
+++ src/composants/Card.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 
-const Card = () => {
+const Card = ({country}) => {
   return (
     <li className="card">
+        <img src={country.flags.svg} alt={"Drapeau" + country.translations.fra.common } />
         <div className="infos">
             <h2>Nom du pays</h2>
         </div>
