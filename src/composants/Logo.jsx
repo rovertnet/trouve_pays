24c7f72c03e1205b0db9d022742cb08c8ddb3ebf
Robert Matundu <matundukabamba@gmail.com>
2023-07-20 18:38:07 +0200

    un autre commit

---
+++ src/composants/Logo.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Logo = () => {
+  return (
+    <div className="logo">
+        <img src="" alt="logo" />
+        <h3>Trouve Pays</h3>
+    </div>
+  )
+}
+
+export default Logo
