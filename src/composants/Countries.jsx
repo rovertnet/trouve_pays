f9ced8e2ad39fc358117eb4189d0066989d4572b
Robert Matundu <matundukabamba@gmail.com>
2023-07-23 10:54:55 +0200

    un autre commit

--- src/composants/Countries.jsx
+++ src/composants/Countries.jsx
@@ -5,6 +5,8 @@ import Card from './Card';
 const Countries = () => {
   const [data, setData] = useState([])
   const [rangeValue, setRangeValue] = useState(36)
+  const radios = ["Afrique", "Amerique", "Asie", "Europe", "Océannie"]
+  const [selectedRadio, setSelectedRadio] = useState("")
   useEffect(() => {
       axios
       .get("https://restcountries.com/v3.1/all")
@@ -14,7 +16,24 @@ const Countries = () => {
   return (
     <div className="countries">
         <ul className="radio-container">
-            <input type="range" min="1" max="250" />
+            <input 
+                type="range" 
+                min="1" 
+                defaultValue={rangeValue}  
+                max="250" 
+                onClick={(e) => setRangeValue(e.target.value)} 
+            />
+            {radios.map((continent) => (
+                <li>
+                    <input 
+                        type="radio" 
+                        id={continent} 
+                        name='continentRadio' 
+                        onChange={(e) => setSelectedRadio(e.target.id)} 
+                    />
+                    <label htmlFor={continent}> {continent} </label>
+                </li>
+            ))}
         </ul>
         <ul>
             {
