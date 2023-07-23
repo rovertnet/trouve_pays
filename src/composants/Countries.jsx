431bddc2d0fb3d4079bfdcb4096ef42350c29200
Robert Matundu <matundukabamba@gmail.com>
2023-07-23 18:52:12 +0200

    un autre commit

--- src/composants/Countries.jsx
+++ src/composants/Countries.jsx
@@ -3,50 +3,49 @@ import React, { useEffect, useState } from 'react'
 import Card from './Card';
 
 const Countries = () => {
-  const [data, setData] = useState([]);
-  const [rangeValue, setRangeValue] = useState(36);
-  const radios = ["Afrique", "Amerique", "Asie", "Europe", "Océannie"];
-  const [selectedRadio, setSelectedRadio] = useState("");
-  useEffect(() => {
-      axios
-      .get("https://restcountries.com/v3.1/all")
-      .then((res) => setData(res.data))
-  }, []);
+    const [data, setData] = useState([]);
+    const [rangeValue, setRangeValue] = useState(36);
+    const [selectedRadio, setSelectedRadio] = useState("");
+    const radios = ["Afrique", "Amerique", "Asie", "Europe", "Océannie"];
+    
+    useEffect(() => {
+        axios
+            .get("https://restcountries.com/v3.1/all")
+            .then((res) => setData(res.data))
+    }, []);
 
-  return (
-    <div className="countries">
-        <ul className="radio-container">
-            <input 
-                type="range" 
-                min="1" 
-                defaultValue={rangeValue}  
-                max="250" 
-                onClick={(e) => setRangeValue(e.target.value)} 
-            />
-            {radios.map((continent) => (
-                <li>
-                    <input 
-                        type="radio" 
-                        id={continent} 
-                        name='continentRadio' 
-                        onChange={(e) => setSelectedRadio(e.target.id)} 
-                    />
-                    <label htmlFor={continent}> {continent} </label>
-                </li>
-            ))}
-        </ul>
-        <ul>
-            {
-                data
+    return (
+        <div className="countries">
+            <ul className="radio-container">
+                <input
+                    type="range"
+                    min="1"
+                    defaultValue={rangeValue}
+                    max="250"
+                    onClick={(e) => setRangeValue(e.target.value)}
+                />
+                {radios.map((continent, index) => (
+                    <li>
+                        <input
+                            type="radio"
+                            id={continent}
+                            name='continentRadio'
+                            onChange={(e) => setSelectedRadio(e.target.id)}
+                        />
+                        <label htmlFor={continent}> {continent} </label>
+                    </li>
+                ))}
+            </ul>
+            <ul>
+                {data
                     .filter((country) => country.continents[0].includes(selectedRadio))
                     .slice(0, rangeValue)
                     .map((country, index) => (
-                    <Card key={index} country={country} />
-                ))
-            }
-        </ul>
-    </div>
-  )
+                        <Card key={index} country={country} />
+                    ))}
+            </ul>
+        </div>
+    )
 }
 
 export default Countries
