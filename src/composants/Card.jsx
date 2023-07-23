a7435740c272a2d8d938566e15dfde37efa41fcf
Robert Matundu <matundukabamba@gmail.com>
2023-07-23 19:25:16 +0200

    un autre commit

--- src/composants/Card.jsx
+++ src/composants/Card.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 
 const Card = ({country}) => {
+    console.log(country);
   return (
     <li className="card">
-          <img src={country.flag.svg} alt={country.translations.fra.common } />
+          <img src={country.flags.svg} alt={country.translations.fra.common } />
         <div className="infos">
               <h2>{country.translations.fra.common}</h2>
             <h4>{country.capital}</h4>
-            <p>Pop : {country.population.toLocaleString()} </p>
+              <p>Pop : {country.population.toLocaleString()} </p>
+              <p>Code : {country.idd.root} </p>
+              <p>Lang : {country.translations.fra.languages} </p>
         </div>
     </li>
   )
