c1f33be622636b8fd17c7ff4d59343ca65542748
Robert Matundu <matundukabamba@gmail.com>
2023-07-28 00:15:19 +0200

    create footer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navigation from '../composants/Navigation'
 import Logo from '../composants/Logo'
 import Countries from '../composants/Countries'
+import Footers from '../composants/Footer'
 
 const Home = () => {
   return (
@@ -9,6 +10,8 @@ const Home = () => {
       <Logo />
       <Navigation />
       <Countries />
+
+      <Footers />
     </div>
   )
 }
