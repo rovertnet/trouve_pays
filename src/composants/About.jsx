9fce058610c3ce0de0ecaea25441f08b129c4bcc
Robert Matundu <matundukabamba@gmail.com>
2023-07-28 01:13:46 +0200

    create footer

---
+++ src/composants/About.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import "./about.css";
+import aboutImg from "../../assets/moi.png";
+import Infos from './Infos';
+
+const About = () => {
+    return (
+        <div className="about section" id="about">
+            <h2 className="section_title">A propos de moi</h2>
+            <span className="section_subtitle">mon introduction</span>
+            <div className="about_container container grid">
+                <img src={aboutImg} alt="" className="about_img" />
+                <div className="about_data">
+                    <Infos />
+                    <p className="about_description">
+                        Ingénieur expérimenté, passionné par
+                        l’univers ICT et son évolution
+                        croissante, j’ambitionne de mettre mes
+                        compétences au service d’une entreprise
+                        à la culture innovante, enthousiaste et
+                        audacieuse. Grâce à un parcours de
+                        formation approfondie et à une somme
+                        d’expériences professionnelles
+                        performante, je suis devenu expert dans
+                        la conception et l’adaptation
+                        d’applications web.
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default About
